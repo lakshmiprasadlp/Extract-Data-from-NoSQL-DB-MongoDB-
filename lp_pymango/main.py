c3d0c1e6aea177a9b1bd8d50d800ecedb582fc49
lakshmiprasadlp <145165461+lakshmiprasadlp@users.noreply.github.com>
2024-03-07 08:58:57 +0000

    i think it is complet but it havind silly eror

--- lp_pymango/main.py
+++ lp_pymango/main.py
@@ -1,11 +1,8 @@
-import lp_pymango
-from lp_pymango import username
-
-"""
+import pandas as pd
 import pymongo
-
 import urllib.parse
 
+from lp_pymango.var.variables import username, password, dbname, colname
 
 # Escape the username and password
 escaped_username = urllib.parse.quote_plus(username)
@@ -16,4 +13,19 @@ uri = f"mongodb+srv://{escaped_username}:{escaped_password}@cluster0.nyhdbb1.mon
 
 # Establish a connection to the MongoDB server
 client = pymongo.MongoClient(uri)
-"""
+
+db = client[dbname]
+collection = db[colname]
+
+# Retrieve all documents from the collection
+documents = collection.find()
+# Convert the MongoDB documents into a DataFrame
+df = pd.DataFrame(documents)
+
+# Specify the full file path including the filename where you want to save the CSV file
+csv_file_path = download_file_path
+
+# Save the DataFrame to a CSV file
+df.to_csv(csv_file_path, index=False)
+
+print(f"CSV file created: {csv_file_path}")
